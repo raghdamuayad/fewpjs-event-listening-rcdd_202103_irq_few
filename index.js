ccc863fcb419e1b86b26c691468ba5cd1536b662
Raghda  Muayad <therose000777@gmail.com>
2021-04-20 20:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const input = document.getElementById('input');
+function addingEventListener() {
+  const input = document.getElementById('input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
-function myFunction() {
-  alert ("Hello World!");
 }
